bdda543b74f47876268ae14121cbf8f852872f2c
DoneHome <donehomer@gmail.com>
2016-10-14 19:01:21 +0800

    函数接口v1.0，待补充

---
+++ ranking.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import sys,os
+import requests
+
+def get_repo_stars(repo):
+    """
+    某个项目的star数
+    """
+    pass
+
+def get_repo_ct(repo):
+    """
+    某个项目的contributors数量
+    """
+    pass
+
+def get_py_repo():
+    """
+    获取python项目
+    """
+    pass
+
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
